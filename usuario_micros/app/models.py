4f6a3986e24c07990d81096005bba3ff9e583913
FabianDz <fabiandz.x@gmail.com>
2023-05-01 21:21:06 -0600

    Reestructuré el microservicio de registro

---
+++ usuario_micros/app/models.py
@@ -0,0 +1,25 @@
+# Importamos módulos
+from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+
+# Clase User (tabla User)
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    tipo_usuario = db.Column(db.String(2), nullable=False)
+    nombre = db.Column(db.String(100), nullable=False)
+    ap_paterno = db.Column(db.String(100), nullable=False)
+    ap_materno = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128))
+    estudiante_pdf = db.Column(db.String(255), nullable=True)
+    cedula_profesional = db.Column(db.String(8), nullable=True)
+    aprobado = db.Column(db.Boolean, default=False)
+
+    # Establece la contraseña del usuario hasheando la contraseña
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    # Verifica la contraseña del usuario
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
---
+++ usuario_micros/run.py
@@ -0,0 +1,11 @@
+# Importamos la aplicación
+from app import create_app
+
+# Instanciamos la app
+app = create_app()
+# Para guardar los archivos pdf
+app.config['UPLOAD_FOLDER'] = 'rutaaponer'
+
+# Corre la aplicación por el puerto 5000
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
