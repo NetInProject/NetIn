78c5aff0af94b5a8d6f0d9efffa572a4b3943a40
Alexmm14 <molinaa757@gmail.com>
2023-05-14 18:49:15 -0600

    Agregar foros y publicaciones

---
+++ foros_micros/app/forms.py
@@ -0,0 +1,21 @@
+# Importamos los módulos
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, SelectField, FileField
+from wtforms.validators import DataRequired, Email, EqualTo, Optional, ValidationError
+from flask_wtf.file import FileAllowed
+from flask import flash
+from app.models import User
+import re
+
+# Formulario de registro de foro
+class formForum(FlaskForm):
+    title = StringField('Nombre del foro', validators=[DataRequired()])
+    description = StringField('Descripción', validators=[DataRequired()])
+    submit = SubmitField('Registrar foro')
+
+# Formulario creacion de publicacion
+class formPublication(FlaskForm):
+    name = StringField('Nombre de la publicación', validators=[DataRequired()])
+    content = StringField('Contenido', validators=[DataRequired()])
+    description = StringField('Descripción', validators=[DataRequired()])
+    submit = SubmitField('Publicar')
--- foros_micros/app/routes.py
+++ foros_micros/app/routes.py
@@ -9,6 +9,9 @@ from flask import redirect, url_for
 from flask import current_app as app
 import os
 from flask import send_file
+from .models import Forum
+from app.forms import formForum, formPublication
+from .models import Publication
 
 
 
@@ -17,7 +20,77 @@ bp = Blueprint('foros_micros', __name__)
 # Ruta por si van al raiz
 @bp.route('/')
 def index():
-    return "Hola mundo"
+    return "Comunicar servicio"
+
+# Ruta para registrar un foro
+
+@bp.route('/registrar_foro', methods=['GET', 'POST'])
+def register_forum():
+    # Instanciamos el formulario de registro de foro
+    form = formForum()
+
+    # Verificamos si el formulario fue enviado
+    if form.validate_on_submit():
+        forum = Forum(
+            title=form.title.data,
+            description=form.description.data
+        )
+
+        # Guardamos la información del foro en la base de datos
+        db.session.add(forum)
+        db.session.commit()
+
+        flash('Foro registrado exitosamente.', 'success')
+
+    return render_template('registro_foro.html', form=form)
+
+
+
+@bp.route('/registrar_publicacion/<int:id_forum>', methods=['GET', 'POST'])
+def registrar_publicacion(id_forum):
+    # Obtener el foro correspondiente al ID
+    forum = Forum.query.get(id_forum)
+
+    # Verificar si el foro existe
+    if not forum:
+        flash('El foro especificado no existe.', 'error')
+        return redirect(url_for('verForos'))
+
+    # Instanciamos el formulario de registro de publicación
+    form = formPublication()
+
+    # Verificamos si el formulario fue enviado
+    if form.validate_on_submit():
+        publication = Publication(
+            name=form.name.data,
+            content=form.content.data,
+            description=form.description.data,
+            forum_id=id_forum  # Asignar el ID del foro a la publicación
+        )
+
+        # Guardamos la información de la publicación en la base de datos
+        db.session.add(publication)
+        db.session.commit()
+
+        flash('Publicación registrada exitosamente.', 'success')
+
+    return render_template('registro_publicacion.html', form=form, id_forum=id_forum)
+
+
+
+
+# Ruta para registrar un foro
+
+@bp.route('/VerForo', methods=['GET', 'POST'])
+def verForos():
+    # Obtener todos los foros de la base de datos
+    foros = Forum.query.all()
+    return render_template('allForos.html', foros=foros)
+
+
+
+
+
 
 
 @bp.route('/verAlumnos', methods=['GET', 'POST'])
@@ -25,3 +98,6 @@ def validar():
     # Obtenemos todos los usuarios que están en la base de datos
     users = User.query.filter_by(tipo_usuario='A', aprobado =0).all()
     return render_template('veralumnos.html', users=users)
+
+
+
